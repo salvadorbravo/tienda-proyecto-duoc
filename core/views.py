6d4a563b3584d61b347c759d10b0cafb581da74e
MichellSmitth <101209000+MichellSmitth@users.noreply.github.com>
2023-05-26 09:06:31 -0400

    Login validación versión prototipo

--- core/views.py
+++ core/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+
+# Validación de usuario en Login
+def login_view(request):
+    if request.method == 'Validate':
+        username = request.Validate['username']
+        password = request.Validate['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('')  # Entre '' debe ir la dirección de login
+        else:
+            messages.error(request, 'Usuario o contraseña incorrectos.')
+    
+    return render(request, 'login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')  
