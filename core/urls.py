014842f61e69129044b65777ae0b9923d0a2351f
salvador <sa.bravo@duocuc.cl>
2023-05-27 20:02:21 -0400

    se agrego el boton para cerrar sesion

--- core/urls.py
+++ core/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('perfil/', views.VistaPerfil.as_view(), name='perfil'),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='core/test-login.html', authentication_form=FormularioLoginUsuario), name='login'),
     path('registro/', views.VistaRegistroCliente.as_view(), name="registrocliente"),
+    path('cerrar-sesion/', auth_views.LogoutView.as_view(next_page='login'), name='cerrar-sesion'), # Cierra la sesion y redirige a la pagina de login
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
